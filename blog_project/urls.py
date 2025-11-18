ff843447fdd2b5437f2b5cb8a1adc0727cc4acd6
arman2255 <armankhan702161@gmail.com>
2025-11-18 13:34:54 +0530

    Update urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include   # 👈 you missed this line before
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # include your blog app’s URLs
+    path('', include('blog.urls')), 
 ]
 
-# To serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
